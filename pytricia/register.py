68c7858393c13fb4a8a9c67f7d7c9e64e974ae64
Julio-Azevedo <julioazevedodev@gmail.com>
2023-06-29 12:29:49 -0300

    feat: alterando o registro para utilizar o tinydb (#1)

--- pytricia/register.py
+++ pytricia/register.py
@@ -1,44 +1,17 @@
-import re as regex
-import pickle
+from tinydb import TinyDB
 
-# função de cadastrar usuários
-def register(users):
+db = TinyDB('users.json')
+
+def register():
+    # Processo de cadastro
     print("Iniciando processo de cadastro")
-    username = input("Informe seu nome: ")
-    while True:
-        date = input("Informe sua data de nascimento: ")
-        if valid_date(date):
-            break
-        else:
-            print("Formato inválido, informe sua data de nascimento como dd/mm/aaaa")
+    username = input("Informe seu nome de usuário: ")
     password = input("Informe sua senha: ")
-    while True:
-        cpf = input("Informe seu CPF: ")
-        if cpf not in users:
-            break
-        else:
-            print("Há um usuário já cadastrado com esse CPF")
-          
-    user = {
-        'username': username,
-        'birth': date,
-        'password': password,
-        'cpf': cpf
-    }
-
-    users[cpf] = user
-    save_user(users)
-    print("Cadastrado com sucesso!")
-
-# função que valida a data inserida   
-def valid_date(date):
-    model = r"\d{2}/\d{2}/\d{4}"
-    if regex.match(model, date):
-        return True
-    else:
-        return False
     
-def save_user(users):
-    with open("users.dat", "ab") as arquivo:
-        users_printed = pickle.dumps(users)
-        arquivo.write(users_printed)
+    # Criando o usuário
+    user = {'username': username, 'password': password}
+
+    # Inserindo usuário no banco
+    db.insert(user)
+    
+    print("Cadastro com sucesso!")
