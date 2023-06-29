628ae719d4e368f95fc79b984b1d1587ae5bf548
Julio-Azevedo <julioazevedodev@gmail.com>
2023-06-29 14:39:38 -0300

    #

---
+++ pytricia/login/login.py
@@ -0,0 +1,21 @@
+import os
+from register.register import db
+from tinydb import Query
+from user_roles import logged
+
+users = db.table('users')
+
+def login():
+    print("Login")
+    # Iniciando processo de login
+    username = input("Informe seu nome de usuário: ")
+    password = input("Informe sua senha: ")
+    # Consultando o banco de dados
+    User = Query()
+    search = users.get((User.username == username) & (User.password == password))
+        
+    if search is not None:
+        os.system("clear||cls")
+        logged(search)
+    else:
+        print("Usuário ou senha inválidos")
