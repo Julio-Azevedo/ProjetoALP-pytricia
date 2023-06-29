8cb5aaa191a4144592f8a4b9616f0bc60c91c279
Julio-Azevedo <julioazevedodev@gmail.com>
2023-06-29 14:01:46 -0300

    feat: mundando a forma de procrurar os usuários dentro do banco (#1)

--- pytricia/login.py
+++ pytricia/login.py
@@ -1,16 +1,23 @@
+import os
 from register import db
 from tinydb import Query
 
+users = db.table('users')
+
 def login():
-    # Iniciando processo de login
     print("Login")
+    # Iniciando processo de login
     username = input("Informe seu nome de usuário: ")
     password = input("Informe sua senha: ")
     # Consultando o banco de dados
-    user = Query()
-    search = db.search((user.username == username) & (user.password == password))
-    
-    if search:
-        print("Sucesso")
+    User = Query()
+    search = users.get((User.username == username) & (User.password == password))
+        
+    if search is not None:
+        os.system("clear||cls")
+        logged(search)
     else:
         print("Usuário ou senha inválidos")
+        
+def logged(search):
+    print(f"Bem vindo a PyTricia {search['username']}")
