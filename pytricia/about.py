c40a6283f5a52f1470ae26cd1486e1d6dc16ecaa
Julio-Azevedo <julioazevedodev@gmail.com>
2023-06-29 12:49:39 -0300

    adicionando comentário ao arquivo (#1)

--- pytricia/about.py
+++ pytricia/about.py
@@ -1,3 +1,4 @@
+# Definando uma função de informações sobre a PyTricia
 def show_info():
     print("Pytricia é um programa que simula uma cigana virtual")
     print("O programa permite que o usuário faça cadastro e login para que possa utilizar as funcionalidadesda cigana virtual")
