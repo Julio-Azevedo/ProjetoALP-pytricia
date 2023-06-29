51489ba4094993a0e21771ee2b9b7a68b4d4eeb2
Julio-Azevedo <julioazevedodev@gmail.com>
2023-06-28 23:49:03 -0300

    feat: adicionado código principal (#1)

---
+++ pytricia/main.py
@@ -0,0 +1,28 @@
+import os
+from mainMenu import show_main_menu
+from register import register
+from about import show_info
+from login import login
+
+# criando dicionario dos usuarios
+users = {}
+
+# chamando o menu
+opt = show_main_menu()
+
+while opt != "0":
+    if opt == "1":
+        os.system("clear||cls")
+        login(users)
+    elif opt == "2":
+        os.system("clear||cls")
+        register(users)
+    elif opt == "3":
+        os.system("clear||cls")
+        show_info()
+    else:
+        print("Opção invalida!")
+    input("\nPressione Enter para voltar ao menu principal")
+    opt = show_main_menu()
+os.system("clear||cls")
+print("Encerando PyTricia...")
