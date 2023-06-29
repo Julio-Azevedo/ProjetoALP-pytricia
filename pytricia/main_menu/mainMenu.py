f85fd559e45998ea6fdef5536176791c701f57bf
Julio-Azevedo <julioazevedodev@gmail.com>
2023-06-29 14:40:21 -0300

    #

---
+++ pytricia/main_menu/mainMenu.py
@@ -0,0 +1,16 @@
+import os
+
+# Definindo a função do menu principal
+def show_main_menu():
+    os.system("clear||cls")
+    print("==================")
+    print("==== PyTricia ====")
+    print("==================")
+    print("\n")
+    print("[1] - Entrar")
+    print("[2] - Cadastrar")
+    print("[3] - Sobre a PyTricia")
+    print("[0] - Sair")
+    print("\n")
+    option = input("Digite a opção desejada: ")
+    return option
