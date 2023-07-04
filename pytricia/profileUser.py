182db247dab3afb1dc5f83d6aca565d6fabbab51
Julio-Azevedo <julioazevedodev@gmail.com>
2023-07-04 14:32:29 -0300

    fix: corrigida a importação

--- pytricia/profileUser.py
+++ pytricia/profileUser.py
@@ -1,6 +1,6 @@
 import os
 from user_menu import user_menu
-from profile_functions import ver_perfil
+from profile_functions import horos
 
 def logado(search, username):
     opt = user_menu(search)
@@ -8,7 +8,7 @@ def logado(search, username):
     while opt != "0":
         if opt == "1":
             os.system("clear||cls")
-            ver_perfil(search)
+            horos()
         elif opt == "2":
             continue
         elif opt == "3":
