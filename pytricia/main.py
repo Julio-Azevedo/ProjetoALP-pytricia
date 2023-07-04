8f618c9d335d96c3d2dcfa3c0aef48b514d72e26
Julio-Azevedo <julioazevedodev@gmail.com>
2023-07-04 13:10:15 -0300

    ajuste no uso do código principal

--- pytricia/main.py
+++ pytricia/main.py
@@ -1,25 +1,23 @@
 import os
-from mainMenu import show_main_menu
+from menu import menu_principal
 from login import login
-from register import register
-from profileUser import logged
+from register import cadastro
+from profileUser import logado
 
 # Chamando o menu principal
-opt = show_main_menu()
+opt = menu_principal()
 
 # Código principal
 while opt != "0":
     if opt == "1":
         os.system("clear||cls")
-        username = login()
-        if username:
-            logged(username)
+        login()
     elif opt == "2":
         os.system("clear||cls")
-        register()
+        cadastro()
     else:
         print("Opção invalida!")
     input("\nPressione Enter para voltar ao menu principal")
-    opt = show_main_menu()
+    opt = menu_principal()
 os.system("clear||cls")
 print("Encerando PyTricia...")
