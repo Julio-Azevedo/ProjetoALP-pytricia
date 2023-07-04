4dadd4cc7908231b2684b06f785eacf879b48fd9
Julio-Azevedo <julioazevedodev@gmail.com>
2023-07-04 14:32:15 -0300

    feat: adicionada a função de horóscopo

--- pytricia/profile_functions.py
+++ pytricia/profile_functions.py
@@ -1,3 +1,35 @@
+import os
+import random
+import datetime
+import json
+
+# Funcionalidade de horóscopo
+def horos():
+    os.system("clear||cls")
+    print("========================================")
+    print("===            Horóscopo             ===")
+    print("========================================")
+    print("=         Selecione uma opção:         =")
+    print("========================================")
+    print("===  [1] - Horóscopo de hoje         ===")
+    print("===  [2] - Horóscopo de amanhã       ===")
+    print("===  [3] - Horóscopo da semana       ===")
+    print("===  [0] - Voltar                    ===")
+    print("========================================")
+    print()
+    opt = input("Digite a opção desejada: ")
+    
+    if opt == "1":
+        with open('horoscope.json') as arquivo:
+            dados = json.load(arquivo)
+            # A variável "frases" está armazenando um dicionario
+            frases = dados['phrases']
+            # "astral" está selecionando uma frase aleatória com a função choice do random, nesta parte, estão sendo pegos os valores da variavel "frases", e então é utilizado a função 'list', para colocalo em lista, e o "['phrase']", garante que acessamos a chave do objeto para obter a frase
+            astral = random.choice(list(frases.values()))['phrase']
+        print(f"Astral do dia: {datetime.date.today()}")
+        print(f"Frase do horóscopo: {astral}")
+
+
 # Funcionalidade de ver o perfil
 def ver_perfil(search):
     print(f"Perfil do usuário")
