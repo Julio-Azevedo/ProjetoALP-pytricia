6dec5c7c8e31284f85339d0a92ed80b03a5769a4
Julio-Azevedo <julioazevedodev@gmail.com>
2023-07-05 12:58:33 -0300

    feat: arquivo que contem as funções de menu

--- pytricia/menu.py
+++ pytricia/menu.py
@@ -1,18 +1,96 @@
 import os
-# Definindo a função do menu principal
-def menu_principal():
+import datetime
+
+
+# ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+
+# Criando a função do menu iniciar
+def menu():
     os.system("clear||cls")
-    print("=============================")
-    print("===       PyTricia        ===")
-    print("=============================")
-    print("=   Selecione uma opção:    =")
-    print("=============================")
-    print("===  [1] - Entrar         ===")
-    print("===  [2] - Cadastrar      ===")
-    print("===  [0] - Sair           ===")
-    print("=============================")
-    print()
-    print("\tAluno: Júlio César Costa de Azevedo")
-    print()
-    opt = input("Digite a opção desejada: ")
-    return opt
+    conteudo = 40
+    menu_opcoes = {
+        "1": "Login",
+        "2": "Cadastro",
+        "0": "Sair"
+    }
+    
+    print(f"+{'-' * conteudo}+")
+    print(f"|{'Bem-vindo a PyTricia': ^{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+    print(f"|{'Menu': ^{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+    for i, j in menu_opcoes.items():
+        print(f"|{f' {i} - {j}':{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+        
+    option = input("Digite a opção desejada: ")
+    return option
+
+
+# ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+
+# Criando a função de menu do usuário
+def menu_usuario(busca):
+    os.system("clear||cls")
+    conteudo = 40
+    menu_opcoes = {
+        "1": "Horoscopo",
+        "2": "Numerologia",
+        "0": "Sair"
+    }
+    
+    print(f"+{'-' * conteudo}+")
+    print(f"|{'Bem-vindo a PyTricia ' + busca['username'].split()[0]:^{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+    print(f"|{'Menu': ^{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+    for i, j in menu_opcoes.items():
+        print(f"|{f' {i} - {j}':{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+        
+    option = input("Digite a opção desejada: ")
+    return option
+
+
+# ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+
+# Criando a função que chama o menu do horoscopo
+def horoscopo_menu():
+    os.system("clear||cls")
+    conteudo = 40
+    menu_opcoes = {
+        "1": "Horoscopo do dia",
+        "2": "Horoscopo de amanhã",
+        "3": "Horoscopo da semana",
+        "0": "Voltar"
+    }
+    
+    print(f"+{'-' * conteudo}+")
+    print(f"|{'Horoscopo': ^{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+    print(f"|{'Menu': ^{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+    for i, j in menu_opcoes.items():
+        print(f"|{f' {i} - {j}':{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+    
+    option = input("Digite a opção desejada: ")
+    return option
+
+
+# ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+
+# Criando função para exibir a tela de mensagem do horoscopo
+def horocopo_mensagem(astral, busca):
+    conteudo = 100
+    data_atual = datetime.date.today()
+    data_formatada = data_atual.strftime('%d/%m/%Y')
+    
+    print(f"+{'-' * conteudo}+")
+    print(f"|{'A mensagem do dia ' + data_formatada + ' para o Signo de ' + busca['sign']:^{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+    print(f"|{astral:^{conteudo}}|")
+    print(f"+{'-' * conteudo}+")    
+    
+
+# ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
