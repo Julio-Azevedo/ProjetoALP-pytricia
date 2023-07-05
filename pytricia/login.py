e3d71e2d899a5c32461eac01689775232c399f4f
Julio-Azevedo <julioazevedodev@gmail.com>
2023-07-05 12:58:09 -0300

    feat: adicionada função de login

--- pytricia/login.py
+++ pytricia/login.py
@@ -1,21 +1,36 @@
 import os
-from register import db
+from cadastro import db
 from tinydb import Query
-from profileUser import logado
+from perfil import perfil
 
-users = db.table('users')
+# ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
 
+usuarios = db.table('users')
+
+
+# ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+
+# Criando a função de login de usuário
 def login():
-    print("Login")
-    # Iniciando processo de login
-    username = input("Informe seu nome de usuário: ")
-    password = input("Informe sua senha: ")
-    # Consultando o banco de dados
-    User = Query()
-    search = users.get((User.username == username) & (User.password == password))
-        
-    if search is not None:
+    os.system("clear||cls")
+    conteudo = 40
+    print(f"+{'-' * conteudo}+")
+    print(f"|{'Login': ^{conteudo}}|")
+    print(f"+{'-' * conteudo}+")
+    
+    # Processo de login
+    login_nome = input("Infome seu nome de usuário: ")
+    login_senha = input("Informe sua senha: ")
+    
+    # Consultando o banco 
+    Usuario = Query()
+    busca = usuarios.get((Usuario.username == login_nome) & (Usuario.password == login_senha))
+    
+    if busca is not None:
         os.system("clear||cls")
-        logado(search, username)
+        perfil(busca)
     else:
-        print("Usuário ou senha inválidos")
+        print("Usuário ou senha invalidos, você será redirecionando!")
+
+
+# ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
