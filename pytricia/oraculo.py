49b90532305cc0f360adc255b107b5d1058073da
Julio-Azevedo <julioazevedodev@gmail.com>
2023-07-09 17:19:01 -0300

    # adicionando a funcionalidade de modificar o perfil de usuário

--- pytricia/oraculo.py
+++ pytricia/oraculo.py
@@ -1,6 +1,7 @@
 import os
-from menu import horoscopo_menu, horocopo_mensagem, numerologia_menu, numerologia_nome_mensagem
+from menu import horoscopo_menu, horocopo_mensagem, numerologia_menu, numerologia_nome_mensagem, menu_config_perfil
 from verifica import verifica_horoscopo, definicao_numerologia, verifica_numerologia
+from config import exibir_perfil, editar_perfil, deletar_perfil
 
 
 # ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
@@ -37,6 +38,32 @@ def numerologia(busca):
         else:
             print("Opção invalida, tente novamente!")
         option = numerologia_menu()
+
+
+# ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
+
+# Criando função de configurar perfil
+def config_perfil(busca):
+    option = menu_config_perfil()
+    deletado = False 
+    
+    while option != "0":
+        if option == "1":
+            os.system("clear||cls")
+            exibir_perfil(busca)
+            input("Pressione ENTER para voltar")
+        elif option == "2":
+            os.system("clear||cls")
+            editar_perfil(busca)
+        elif option == "3":
+            os.system("clear||cls")
+            deletado = deletar_perfil(busca)
+            break
+        else:
+            print("Opção invalida, tente novamente!") 
+        option = menu_config_perfil()
+        
+    return deletado
 
 
 # ´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´
